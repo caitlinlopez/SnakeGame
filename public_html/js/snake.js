bea503d4272644a43078aecc805c735b03092414
caitlinlopez <CaitlinL128@hotmail.com>
2015-11-02 10:12:22 -0800

    no message

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -7,6 +7,12 @@ var screenHeight;
 function gameInitialize() {
     var canvas = document.getElementsById("game-screen");
     context = canvas.getContext("2d");
+    
+    screenWidth = window.innerWidth;
+    screenHeight = window.innerHeight;
+    
+    canvas.width = screenWidth;
+    canvas.height = screenHeight;
 }
 
 function gameLoop() {
