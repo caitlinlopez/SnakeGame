5d66881513a8daa83154764ec42b26239f109d4a
caitlinlopez <CaitlinL128@hotmail.com>
2015-11-12 08:52:54 -0800

    no message

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -87,7 +87,7 @@ function snakeUpdate() {
     if(snakeDirection == "down") {
         snakeHeadY++;
     }
-    else {
+    else if(snakeDirection == "right"){
         snakeHeadX++;
     }
     
@@ -130,7 +130,10 @@ function setFoodPosition() {
 function keyboardHandler(event) {
     console.log(event);
     
-    if(event.keyCode == "68") {
+    if(event.keyCode == "39") {
         snakeDirection = "right";
+    }
+    else if(event.keyCode == "40") {
+        snakeDirection = "down";
     }
 }
