552f18c31f88f693dc311ff085f6c677cbb8c5bd
caitlinlopez <CaitlinL128@hotmail.com>
2015-11-12 08:41:38 -0800

    no message

--- public_html/js/snake.js
+++ public_html/js/snake.js
@@ -123,6 +123,14 @@ function setFoodPosition() {
     food.y = randomY;
 }
 
+/*-------------------------------------------------------
+ * Input Functions
+ * ------------------------------------------------------
+ */
 function keyboardHandler(event) {
     console.log(event);
+    
+    if(event.keyCode == "68") {
+        snakeDirection = "right";
+    }
 }
